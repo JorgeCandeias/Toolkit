f23f1d6c612cb5636ab1f502c890fa00ab3b828f
jorge <jorge.candeias@outcompute.com>
2023-02-11 19:07:53 +0000

    WIP

---
+++ Tests/Outcompute.Toolkit.HighPerformance.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Outcompute.Toolkit.HighPerformance.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
