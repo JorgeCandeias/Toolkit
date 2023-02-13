d829cfe11f553f76b3a17a6e609b250ac828d4d4
Jorge Candeias <jorge.candeias@outcompute.com>
2023-02-13 18:47:33 +0000

    Added ArrayPoolBufferWriterEnumerableExtensions

---
+++ Sources/Outcompute.Toolkit.HighPerformance/Extensions/ArrayPoolBufferWriterEnumerableExtensions.cs
@@ -0,0 +1,175 @@
+﻿using CommunityToolkit.HighPerformance;
+
+namespace Outcompute.Toolkit.HighPerformance.Extensions;
+
+public static class ArrayPoolBufferWriterEnumerableExtensions
+{
+    /// <summary>
+    /// Enumerates the specified <paramref name="source"/> into a new <see cref="ArrayPoolBufferWriter{T}"/> of the correct size.
+    /// </summary>
+    public static ArrayPoolBufferWriter<T> ToArrayPoolBufferWriter<T>(this IEnumerable<T> source)
+    {
+        Guard.IsNotNull(source);
+
+        return source switch
+        {
+            T[] array => ToArrayPoolBufferWriterFromArray(array),
+            List<T> list => ToArrayPoolBufferWriterFromList(list),
+            Queue<T> queue => ToArrayPoolBufferWriterFromQueue(queue),
+            Stack<T> stack => ToArrayPoolBufferWriterFromStack(stack),
+            _ => ToArrayPoolBufferWriterFromEnumerable(source)
+        };
+    }
+
+    /// <summary>
+    /// Fast path of <see cref="ToArrayPoolBufferWriter{T}(IEnumerable{T})"/> for <see cref="Array"/>.
+    /// </summary>
+    /// <remarks>
+    /// Arrays are internally copied in a single block copy operation.
+    /// </remarks>
+    private static ArrayPoolBufferWriter<T> ToArrayPoolBufferWriterFromArray<T>(T[] source)
+    {
+        if (source.Length == 0)
+        {
+            return new ArrayPoolBufferWriter<T>();
+        }
+
+        var owner = new ArrayPoolBufferWriter<T>(source.Length);
+
+        var span = owner.GetSpan(source.Length);
+        source.CopyTo(span);
+        owner.Advance(source.Length);
+
+        return owner;
+    }
+
+    /// <summary>
+    /// Fast path of <see cref="ToArrayPoolBufferWriter{T}(IEnumerable{T})"/> for <see cref="List{T}"/>.
+    /// </summary>
+    /// /// <remarks>
+    /// Lists are internally copied in a single block copy operation.
+    /// </remarks>
+    private static ArrayPoolBufferWriter<T> ToArrayPoolBufferWriterFromList<T>(List<T> source)
+    {
+        if (source.Count == 0)
+        {
+            return new ArrayPoolBufferWriter<T>();
+        }
+
+        var owner = new ArrayPoolBufferWriter<T>(source.Count);
+
+        var span = owner.GetSpan(source.Count);
+        CollectionsMarshal.AsSpan(source).CopyTo(span);
+        owner.Advance(source.Count);
+
+        return owner;
+    }
+
+    /// <summary>
+    /// Fast path of <see cref="ToArrayPoolBufferWriter{T}(IEnumerable{T})"/> for <see cref="Queue{T}"/>.
+    /// </summary>
+    /// <remarks>
+    /// Queues are internally copied in up to two block copy operations.
+    /// </remarks>
+    private static ArrayPoolBufferWriter<T> ToArrayPoolBufferWriterFromQueue<T>(Queue<T> source)
+    {
+        if (source.Count == 0)
+        {
+            return new ArrayPoolBufferWriter<T>();
+        }
+
+        var owner = new ArrayPoolBufferWriter<T>(source.Count);
+
+        if (!MemoryMarshal.TryGetArray<T>(owner.GetMemory(source.Count), out var segment))
+        {
+            // should never happen
+            return ThrowHelper.ThrowInvalidOperationException<ArrayPoolBufferWriter<T>>();
+        }
+
+        source.CopyTo(segment.Array!, 0);
+        owner.Advance(source.Count);
+
+        return owner;
+    }
+
+    /// <summary>
+    /// Fast path of <see cref="ToArrayPoolBufferWriter{T}(IEnumerable{T})"/> for <see cref="Stack{T}"/>.
+    /// </summary>
+    /// <remarks>
+    /// Stacks are internally copied in a fast while loop.
+    /// </remarks>
+    private static ArrayPoolBufferWriter<T> ToArrayPoolBufferWriterFromStack<T>(Stack<T> source)
+    {
+        if (source.Count == 0)
+        {
+            return new ArrayPoolBufferWriter<T>();
+        }
+
+        var owner = new ArrayPoolBufferWriter<T>(source.Count);
+
+        if (!MemoryMarshal.TryGetArray<T>(owner.GetMemory(source.Count), out var segment))
+        {
+            // should never happen
+            return ThrowHelper.ThrowInvalidOperationException<ArrayPoolBufferWriter<T>>();
+        }
+
+        source.CopyTo(segment.Array!, 0);
+        owner.Advance(source.Count);
+
+        return owner;
+    }
+
+    /// <summary>
+    /// Fallback of <see cref="ToArrayPoolBufferWriter{T}(IEnumerable{T})"/> for enumerables.
+    /// </summary>
+    private static ArrayPoolBufferWriter<T> ToArrayPoolBufferWriterFromEnumerable<T>(IEnumerable<T> source)
+    {
+        if (source.TryGetNonEnumeratedCount(out var count))
+        {
+            return ToArrayPoolBufferWriterFromEnumerableWithKnownCount(source, count);
+        }
+        else
+        {
+            return ToArrayPoolBufferWriterFromEnumerableWithUnknownCount(source);
+        }
+    }
+
+    /// <summary>
+    /// Fallback of <see cref="ToArrayPoolBufferWriter{T}(IEnumerable{T})"/> for enumerables with a known count.
+    /// </summary>
+    private static ArrayPoolBufferWriter<T> ToArrayPoolBufferWriterFromEnumerableWithKnownCount<T>(IEnumerable<T> source, int count)
+    {
+        if (count == 0)
+        {
+            return new ArrayPoolBufferWriter<T>();
+        }
+
+        var owner = new ArrayPoolBufferWriter<T>();
+        var span = owner.GetSpan(count);
+        var i = 0;
+
+        foreach (var item in source)
+        {
+            span[i++] = item;
+        }
+
+        owner.Advance(count);
+
+        return owner;
+    }
+
+    /// <summary>
+    /// Fallback of <see cref="ToArrayPoolBufferWriter{T}(IEnumerable{T})"/> for enumerables with an unknown count.
+    /// </summary>
+    private static ArrayPoolBufferWriter<T> ToArrayPoolBufferWriterFromEnumerableWithUnknownCount<T>(IEnumerable<T> source)
+    {
+        var owner = new ArrayPoolBufferWriter<T>();
+
+        foreach (var item in source)
+        {
+            owner.Write(item);
+        }
+
+        return owner;
+    }
+}
---
+++ Tests/Outcompute.Toolkit.HighPerformance.Tests/Extensions/ArrayPoolBufferWriterEnumerableExtensionsTests.cs
@@ -0,0 +1,184 @@
+﻿using CommunityToolkit.HighPerformance;
+using Outcompute.Toolkit.HighPerformance.Extensions;
+
+namespace Outcompute.Toolkit.HighPerformance.Tests.Extensions;
+
+public static class ArrayPoolBufferWriterEnumerableExtensionsTests
+{
+    [Fact]
+    public static void ThrowsOnNullSource()
+    {
+        // arrange
+        IEnumerable<int> source = null!;
+
+        // act
+        var ex = Assert.Throws<ArgumentNullException>(() => source.ToArrayPoolBufferWriter());
+
+        // assert
+        Assert.Equal(nameof(source), ex.ParamName);
+    }
+
+    [Fact]
+    public static void ConvertsEmptyArray()
+    {
+        // arrange
+        var source = Array.Empty<int>();
+
+        // act
+        using var result = source.ToArrayPoolBufferWriter();
+
+        // act
+        Assert.Equal(source.Length, result.WrittenCount);
+    }
+
+    [Fact]
+    public static void ConvertsFilledArray()
+    {
+        // arrange
+        var source = Enumerable.Range(1, 100).ToArray();
+
+        // act
+        using var result = source.ToArrayPoolBufferWriter();
+
+        // act
+        Assert.Equal(source.Length, result.WrittenCount);
+        Assert.Equal(source, result.WrittenSpan.ToArray());
+    }
+
+    [Fact]
+    public static void ConvertsEmptyList()
+    {
+        // arrange
+        var source = new List<int>();
+
+        // act
+        using var result = source.ToArrayPoolBufferWriter();
+
+        // act
+        Assert.Equal(source.Count, result.WrittenCount);
+    }
+
+    [Fact]
+    public static void ConvertsFilledList()
+    {
+        // arrange
+        var source = Enumerable.Range(1, 100).ToList();
+
+        // act
+        using var result = source.ToArrayPoolBufferWriter();
+
+        // act
+        Assert.Equal(source.Count, result.WrittenCount);
+        Assert.Equal(source, result.WrittenSpan.ToArray());
+    }
+
+    [Fact]
+    public static void ConvertsEmptyQueue()
+    {
+        // arrange
+        var source = new Queue<int>();
+
+        // act
+        using var result = source.ToArrayPoolBufferWriter();
+
+        // act
+        Assert.Equal(source.Count, result.WrittenCount);
+    }
+
+    [Fact]
+    public static void ConvertsFilledQueue()
+    {
+        // arrange
+        var source = new Queue<int>();
+        foreach (var item in Enumerable.Range(1, 100))
+        {
+            source.Enqueue(item);
+        }
+
+        // act
+        using var result = source.ToArrayPoolBufferWriter();
+
+        // act
+        Assert.Equal(source.Count, result.WrittenCount);
+        Assert.Equal(source, result.WrittenSpan.ToArray());
+    }
+
+    [Fact]
+    public static void ConvertsEmptyStack()
+    {
+        // arrange
+        var source = new Stack<int>();
+
+        // act
+        using var result = source.ToArrayPoolBufferWriter();
+
+        // act
+        Assert.Equal(source.Count, result.WrittenCount);
+    }
+
+    [Fact]
+    public static void ConvertsFilledStack()
+    {
+        // arrange
+        var source = new Stack<int>();
+        foreach (var item in Enumerable.Range(1, 100))
+        {
+            source.Push(item);
+        }
+
+        // act
+        using var result = source.ToArrayPoolBufferWriter();
+
+        // act
+        Assert.Equal(source.Count, result.WrittenCount);
+        Assert.Equal(source, result.WrittenSpan.ToArray());
+    }
+
+    [Fact]
+    public static void ConvertsEmptyEnumerableWithKnownCount()
+    {
+        // arrange
+        var source = new HashSet<int>();
+
+        // act
+        using var result = source.ToArrayPoolBufferWriter();
+
+        // act
+        Assert.Equal(source.Count, result.WrittenCount);
+    }
+
+    [Fact]
+    public static void ConvertsFilledEnumerableWithKnownCount()
+    {
+        // arrange
+        var source = Enumerable.Range(1, 100).ToHashSet();
+
+        // act
+        using var result = source.ToArrayPoolBufferWriter();
+
+        // act
+        Assert.Equal(source.Count, result.WrittenCount);
+        Assert.Equal(source, result.WrittenSpan.ToArray());
+    }
+
+    [Fact]
+    public static void ConvertsEnumerableWithUnknownCount()
+    {
+        // arrange
+        static IEnumerable<int> Generate()
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                yield return i;
+            }
+        }
+        var source = Generate();
+
+        // act
+        using var result = source.ToArrayPoolBufferWriter();
+
+        // act
+        Assert.Equal(source.Count(), result.WrittenCount);
+        Assert.Equal(source, result.WrittenSpan.ToArray());
+    }
+}
