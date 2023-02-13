e60daba16596056e3e182c69a8f8ef02290671b4
Jorge Candeias <jorge.candeias@outcompute.com>
2023-02-13 18:01:09 +0000

    Added SpanOwnerEnumerableExtensions

---
+++ Sources/Outcompute.Toolkit.HighPerformance/Extensions/SpanOwnerEnumerableExtensions.cs
@@ -0,0 +1,200 @@
+﻿namespace Outcompute.Toolkit.HighPerformance.Extensions;
+
+public static class SpanOwnerEnumerableExtensions
+{
+    /// <summary>
+    /// The starting buffer length to use.
+    /// The minimum array size managed by the shared <see cref="ArrayPool{T}"/> is 16.
+    /// </summary>
+    private const int DefaultBufferLength = 16;
+
+    /// <summary>
+    /// Enumerates the specified <paramref name="source"/> into a new <see cref="SpanOwner{T}{T}"/> of the correct size.
+    /// </summary>
+    public static SpanOwner<T> ToSpanOwner<T>(this IEnumerable<T> source)
+    {
+        Guard.IsNotNull(source);
+
+        return source switch
+        {
+            T[] array => ToSpanOwnerFromArray(array),
+            List<T> list => ToSpanOwnerFromList(list),
+            Queue<T> queue => ToSpanOwnerFromQueue(queue),
+            Stack<T> stack => ToSpanOwnerFromStack(stack),
+            _ => ToSpanOwnerFromEnumerable(source)
+        };
+    }
+
+    /// <summary>
+    /// Fast path of <see cref="ToSpanOwner{T}(IEnumerable{T})"/> for <see cref="Array"/>.
+    /// </summary>
+    /// <remarks>
+    /// Arrays are internally copied in a single block copy operation.
+    /// </remarks>
+    private static SpanOwner<T> ToSpanOwnerFromArray<T>(T[] source)
+    {
+        if (source.Length == 0)
+        {
+            return SpanOwner<T>.Empty;
+        }
+
+        var owner = SpanOwner<T>.Allocate(source.Length);
+
+        source.CopyTo(owner.Span);
+
+        return owner;
+    }
+
+    /// <summary>
+    /// Fast path of <see cref="ToSpanOwner{T}(IEnumerable{T})"/> for <see cref="List{T}"/>.
+    /// </summary>
+    /// /// <remarks>
+    /// Lists are internally copied in a single block copy operation.
+    /// </remarks>
+    private static SpanOwner<T> ToSpanOwnerFromList<T>(List<T> source)
+    {
+        if (source.Count == 0)
+        {
+            return SpanOwner<T>.Empty;
+        }
+
+        var owner = SpanOwner<T>.Allocate(source.Count);
+        var target = owner.DangerousGetArray().Array!;
+
+        source.CopyTo(target);
+
+        return owner;
+    }
+
+    /// <summary>
+    /// Fast path of <see cref="ToSpanOwner{T}(IEnumerable{T})"/> for <see cref="Queue{T}"/>.
+    /// </summary>
+    /// <remarks>
+    /// Queues are internally copied in up to two block copy operations.
+    /// </remarks>
+    private static SpanOwner<T> ToSpanOwnerFromQueue<T>(Queue<T> source)
+    {
+        if (source.Count == 0)
+        {
+            return SpanOwner<T>.Empty;
+        }
+
+        var owner = SpanOwner<T>.Allocate(source.Count);
+        var target = owner.DangerousGetArray().Array!;
+        source.CopyTo(target, 0);
+
+        return owner;
+    }
+
+    /// <summary>
+    /// Fast path of <see cref="ToSpanOwner{T}(IEnumerable{T})"/> for <see cref="Stack{T}"/>.
+    /// </summary>
+    /// <remarks>
+    /// Stacks are internally copied in a fast while loop.
+    /// </remarks>
+    private static SpanOwner<T> ToSpanOwnerFromStack<T>(Stack<T> source)
+    {
+        if (source.Count == 0)
+        {
+            return SpanOwner<T>.Empty;
+        }
+
+        var owner = SpanOwner<T>.Allocate(source.Count);
+        var target = owner.DangerousGetArray().Array!;
+        source.CopyTo(target, 0);
+
+        return owner;
+    }
+
+    /// <summary>
+    /// Fallback of <see cref="ToSpanOwner{T}(IEnumerable{T})"/> for enumerables.
+    /// </summary>
+    private static SpanOwner<T> ToSpanOwnerFromEnumerable<T>(IEnumerable<T> source)
+    {
+        if (source.TryGetNonEnumeratedCount(out var count))
+        {
+            return ToSpanOwnerFromEnumerableWithKnownCount(source, count);
+        }
+        else
+        {
+            return ToSpanOwnerFromEnumerableWithUnknownCount(source);
+        }
+    }
+
+    /// <summary>
+    /// Fallback of <see cref="ToSpanOwner{T}(IEnumerable{T})"/> for enumerables with a known count.
+    /// </summary>
+    private static SpanOwner<T> ToSpanOwnerFromEnumerableWithKnownCount<T>(IEnumerable<T> source, int count)
+    {
+        if (count == 0)
+        {
+            return SpanOwner<T>.Empty;
+        }
+
+        var owner = SpanOwner<T>.Allocate(count);
+        var span = owner.Span;
+        var i = 0;
+
+        foreach (var item in source)
+        {
+            span[i++] = item;
+        }
+
+        return owner;
+    }
+
+    /// <summary>
+    /// Fallback of <see cref="ToSpanOwner{T}(IEnumerable{T})"/> for enumerables with an unknown count.
+    /// </summary>
+    private static SpanOwner<T> ToSpanOwnerFromEnumerableWithUnknownCount<T>(IEnumerable<T> source)
+    {
+        // grab the starting buffer
+        var temp = ArrayPool<T>.Shared.Rent(DefaultBufferLength);
+        var i = 0;
+
+        // enumerate while growing the buffer
+        foreach (var item in source)
+        {
+            // grow the buffer as needed
+            if (i == temp.Length)
+            {
+                temp = Grow(temp, i);
+            }
+
+            temp[i++] = item;
+        }
+
+        // copy the temp buffer into the memory owner
+        var owner = SpanOwner<T>.Allocate(i);
+        temp.AsSpan(..i).CopyTo(owner.Span);
+        ArrayPool<T>.Shared.Return(temp);
+
+        return owner;
+
+        // grows the temp buffer
+        static T[] Grow(T[] temp, int i)
+        {
+            // break if growth is not possible
+            if (i == Array.MaxLength)
+            {
+                return ThrowHelper.ThrowInsufficientMemoryException<T[]>($"Source contains more than {Array.MaxLength} items");
+            }
+
+            // double the length while clamping overflow
+            var length = (int)Math.Min((uint)temp.Length * 2, int.MaxValue);
+
+            // grow to at least the first bucket size above zero
+            length = Math.Max(length, DefaultBufferLength);
+
+            // grow to at most the max size of an array
+            length = Math.Min(length, Array.MaxLength);
+
+            // swap the buffers
+            var other = ArrayPool<T>.Shared.Rent(length);
+            Array.Copy(temp, other, i);
+            ArrayPool<T>.Shared.Return(temp);
+
+            return other;
+        }
+    }
+}
