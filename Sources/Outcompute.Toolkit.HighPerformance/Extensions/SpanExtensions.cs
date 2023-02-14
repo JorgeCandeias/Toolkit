105fbacec1c408234ea7341dab3d3a598fa774d3
Jorge Candeias <jorge.candeias@outcompute.com>
2023-02-14 22:49:56 +0000

    Added SpanExtensions

---
+++ Sources/Outcompute.Toolkit.HighPerformance/Extensions/SpanExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Outcompute.Toolkit.HighPerformance.Extensions;
+
+public static class SpanExtensions
+{
+    /// Uses the shared <see cref="StringPool"/> to get or created a <see cref="string"/> from the specified <see cref="ReadOnlySpan{char}"/>.
+    public static string ToPooledString(this ReadOnlySpan<char> span)
+    {
+        return StringPool.Shared.GetOrAdd(span);
+    }
+
+    /// Uses the shared <see cref="StringPool"/> to get or created a <see cref="string"/> from the specified <see cref="Span{char}"/>.
+    public static string ToPooledString(this Span<char> span)
+    {
+        return StringPool.Shared.GetOrAdd(span);
+    }
+
+    /// Uses the shared <see cref="StringPool"/> to get or created a <see cref="string"/> from the specified <see cref="ReadOnlySpan{byte}"/>.
+    public static string ToPooledString(this ReadOnlySpan<byte> span, Encoding encoding)
+    {
+        return StringPool.Shared.GetOrAdd(span, encoding);
+    }
+
+    /// Uses the shared <see cref="StringPool"/> to get or created a <see cref="string"/> from the specified <see cref="Span{byte}"/>.
+    public static string ToPooledString(this Span<byte> span, Encoding encoding)
+    {
+        return StringPool.Shared.GetOrAdd(span, encoding);
+    }
+}
---
+++ Tests/Outcompute.Toolkit.HighPerformance.Tests/Extensions/SpanExtensionsTests.cs
@@ -0,0 +1,66 @@
+﻿using CommunityToolkit.HighPerformance;
+using Outcompute.Toolkit.HighPerformance.Extensions;
+using System.Text;
+
+namespace Outcompute.Toolkit.HighPerformance.Tests.Extensions;
+
+public class SpanExtensionsTests
+{
+    [Fact]
+    public void ConvertsReadOnlySpanChar()
+    {
+        // arrange
+        var array = new char[] { 'A', 'B', 'C' };
+        ReadOnlySpan<char> span = array.AsSpan();
+
+        // act
+        var result = span.ToPooledString();
+
+        // assert
+        Assert.Equal("ABC", result);
+    }
+
+    [Fact]
+    public void ConvertsSpanChar()
+    {
+        // arrange
+        var array = new char[] { 'A', 'B', 'C' };
+        Span<char> span = array.AsSpan();
+
+        // act
+        var result = span.ToPooledString();
+
+        // assert
+        Assert.Equal("ABC", result);
+    }
+
+    [Fact]
+    public void ConvertsReadOnlySpanByte()
+    {
+        // arrange
+        var encoding = Encoding.UTF8;
+        var array = encoding.GetBytes("ABC");
+        ReadOnlySpan<byte> span = array.AsSpan();
+
+        // act
+        var result = span.ToPooledString(encoding);
+
+        // assert
+        Assert.Equal("ABC", result);
+    }
+
+    [Fact]
+    public void ConvertsSpanByte()
+    {
+        // arrange
+        var encoding = Encoding.UTF8;
+        var array = encoding.GetBytes("ABC");
+        Span<byte> span = array.AsSpan();
+
+        // act
+        var result = span.ToPooledString(encoding);
+
+        // assert
+        Assert.Equal("ABC", result);
+    }
+}
