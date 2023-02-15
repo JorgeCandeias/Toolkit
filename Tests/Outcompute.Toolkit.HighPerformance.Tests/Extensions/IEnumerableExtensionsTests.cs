314c224692ac414f3f8d3711eb06ee71d4e96482
jorge <jorge.candeias@outcompute.com>
2023-02-15 15:42:21 +0000

    Added tests

---
+++ Tests/Outcompute.Toolkit.HighPerformance.Tests/Extensions/IEnumerableExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Outcompute.Toolkit.HighPerformance.Tests.Extensions;
+
+public class IEnumerableExtensionsTests
+{
+    [Fact]
+    public void CopiesFromKnownCollection()
+    {
+        // arrange
+        var source = (IEnumerable<int>)new[] { 1, 2, 3 };
+        var target = new int[3];
+
+        // act
+        source.CopyTo(target, 0);
+
+        // assert
+        Assert.Equal(source, target);
+    }
+
+    [Fact]
+    public void CopiesFromUnknownEnumerable()
+    {
+        // arrange
+        var source = Enumerable.Range(1, 3);
+        var target = new int[3];
+
+        // act
+        source.CopyTo(target, 0);
+
+        // assert
+        Assert.Equal(source, target);
+    }
+}
