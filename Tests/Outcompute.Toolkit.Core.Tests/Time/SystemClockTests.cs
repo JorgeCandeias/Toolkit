8ec0eb4375fda7ded04de0ed557e9edb686d2f43
Jorge Candeias <jorge.candeias@outcompute.com>
2023-02-15 23:08:04 +0000

    Added tests

--- Tests/Outcompute.Toolkit.Core.Tests/Time/SystemClockTests.cs
+++ Tests/Outcompute.Toolkit.Core.Tests/Time/SystemClockTests.cs
@@ -1,4 +1,6 @@
-﻿namespace Outcompute.Toolkit.Core.Tests.Time;
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Outcompute.Toolkit.Core.Tests.Time;
 
 public class SystemClockTests
 {
@@ -14,5 +16,19 @@ public class SystemClockTests
 
         // assert
         Assert.InRange(result, now.Subtract(TimeSpan.FromSeconds(1)), now.Add(TimeSpan.FromSeconds(1)));
+    }
+
+    [Fact]
+    public void AddsToServiceCollection()
+    {
+        // arrange
+        var services = new ServiceCollection();
+
+        // act
+        var result = services.AddSystemClock();
+
+        // assert
+        Assert.Same(services, result);
+        Assert.NotEmpty(services);
     }
 }
