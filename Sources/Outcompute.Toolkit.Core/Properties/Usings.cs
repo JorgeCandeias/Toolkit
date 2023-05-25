8af80ccf2b98ee40f8078c3bea25fbcf737671d5
jorge <jorge.candeias@outcompute.com>
2023-05-25 19:00:04 +0100

    Removed redundant projects

--- Sources/Outcompute.Toolkit.Core/Properties/Usings.cs
+++
@@ -1,7 +0,0 @@
-﻿global using CommunityToolkit.Diagnostics;
-global using Microsoft.Extensions.DependencyInjection;
-global using System.Collections;
-global using System.Collections.Concurrent;
-global using System.ComponentModel;
-global using System.Data;
-global using System.Runtime.InteropServices;
